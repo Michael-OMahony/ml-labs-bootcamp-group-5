a2330eb3220b01a747109bbdcc4bf45d84bc0557
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2021-10-05 19:08:45 +0530

    test cases for baseline added!

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from src.featutils import aggregate_features_from_folder
 import src.features.t62020 as t62020
+import src.features.baseline as fbase
 from src.datapoint import DataPoint
 import pandas as pd
 
@@ -15,7 +16,9 @@ def test_20t6():
         "data/tc4tl_training_data_v1/tc4tl/docs/tc4tl_train_key.tsv", sep="\t")
     aggregate_features_from_folder(
         folder, key, feat_fn=t62020.extract_features_from_file,
-        postproc_fn=t62020.postproc_feature_frame)
+        postproc_fn=t62020.postproc_feature_frame,
+        predictors=t62020.PREDICTORS, target=t62020.TARGET,
+        testing=True)
 
 
 def test_devset():
@@ -25,4 +28,38 @@ def test_devset():
     aggregate_features_from_folder(
         folder, key, feat_fn=t62020.extract_features_from_file,
         postproc_fn=t62020.postproc_feature_frame,
-        predictors=t62020.PREDICTORS, target=t62020.TARGET)
+        predictors=t62020.PREDICTORS, target=t62020.TARGET,
+        testing=True)
+
+
+def test_baseline():
+    folder = "data/tc4tl_training_data_v1/tc4tl/data/train/"
+    key = pd.read_csv(
+        "data/tc4tl_training_data_v1/tc4tl/docs/tc4tl_train_key.tsv", sep="\t")
+    aggregate_features_from_folder(
+        folder, key, feat_fn=fbase.extract_features_from_file,
+        postproc_fn=fbase.postproc_feature_dicts,
+        predictors=fbase.get_predictors, target=fbase.TARGET,
+        testing=True)
+
+
+def test_baseline_dev():
+    key = pd.read_csv(
+        'data/tc4tl_data_v5/tc4tl/docs/tc4tl_dev_key.tsv', sep='\t')
+    folder = 'data/tc4tl_data_v5/tc4tl/data/dev/'
+    aggregate_features_from_folder(
+        folder, key, feat_fn=fbase.extract_features_from_file,
+        postproc_fn=fbase.postproc_feature_dicts,
+        predictors=fbase.get_predictors, target=fbase.TARGET,
+        testing=True)
+
+
+def test_baseline_train():
+    folder = "data/tc4tl_training_data_v1/tc4tl/data/train/"
+    key = pd.read_csv(
+        "data/tc4tl_training_data_v1/tc4tl/docs/tc4tl_train_key.tsv", sep="\t")
+    aggregate_features_from_folder(
+        folder, key, feat_fn=fbase.extract_features_from_file,
+        postproc_fn=fbase.postproc_feature_dicts,
+        predictors=fbase.get_predictors, target=fbase.TARGET,
+        testing=True)
