847285bb7c0a02a4473aa5a3adbc2bdb7be17817
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2021-10-20 18:39:06 +0530

    fixes bug in `read_chirp_sequence_from_file`

--- src/features/common.py
+++ src/features/common.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
+TARGET = "Distance"
+
 
 def read_chirp_sequence_from_file(filepath):
     """Return a sequence of values containing average RSSI reading of each chirp
     (Reads from file directly)
     """
     data, chirps, chirp = [], [], []
-    for line in (
-        open("data/tc4tl_training_data_v1/tc4tl/data/train/aaadbuvp_tc4tl20.csv")
-        .read()
-        .split("\n")[7:]
-    ):
+    for line in open(filepath).read().split("\n")[7:]:
         if "Bluetooth" in line:
             t, _, rssi = line.split(",")
             item = {"t": float(t), "rssi": float(rssi)}
@@ -26,3 +24,8 @@ def read_chirp_sequence_from_file(filepath):
 
 def postproc_default(feats, encoders={}):
     return pd.DataFrame(feats), encoders
+
+
+def get_predictors_default(dataset):
+    return [col for col in dataset.columns
+            if col not in ['Distance', 'fileid', 'CoarseGrain']]
