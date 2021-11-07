0e8cb51fd2985f047e390b9a29a00389b2ad6018
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2021-11-07 20:03:26 +0530

    adds RFP hyperparams

--- src/hyperopt.py
+++ src/hyperopt.py
@@ -1,7 +1,12 @@
 from hpsklearn import HyperoptEstimator
-from sklearn.ensemble import (AdaBoostClassifier, BaggingClassifier,
-                              ExtraTreesClassifier, GradientBoostingClassifier,
-                              RandomForestClassifier, RandomForestRegressor)
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    BaggingClassifier,
+    ExtraTreesClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+    RandomForestRegressor,
+)
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.linear_model.ridge import RidgeClassifier
 from sklearn.linear_model.stochastic_gradient import SGDClassifier
@@ -29,21 +34,33 @@ CLASSIFIERS = [
 ]
 
 
-RF_PARAM_GRID = {'bootstrap': [True],
-                 'max_depth': [80, 90, 100, 110],
-                 'max_features': [2, 3],
-                 'min_samples_leaf': [3, 4, 5],
-                 'min_samples_split': [8, 10, 12],
-                 'n_estimators': [100, 200, 300, 1000]
-                 }
+RF_PARAM_GRID = {
+    "bootstrap": [True],
+    "max_depth": [80, 90, 100, 110],
+    "max_features": [2, 3],
+    "min_samples_leaf": [3, 4, 5],
+    "min_samples_split": [8, 10, 12],
+    "n_estimators": [100, 200, 300, 1000],
+}
+
+RFP_PARAMS = {
+    "LinearApprox": {"Pt_dBm": -61.02, "n": 2.187},
+    "Friis": dict(Pt_dBm=0.0, Gt_dBi=1.0, Gr_dBi=1.0, f=2.4e9, L=1.0, n=2.0),
+    "LogNormal": dict(
+        Pt_dBm=-20.0, Gt_dBi=1.0, Gr_dBi=1.0, f=2.4e9, d0=1.0, L=1.0, sigma=2.0, n=2
+    ),
+}
 
 
 def optimize(model, train_features, train_labels, param_grid=None):
     if param_grid is None:
-        if isinstance(model, RandomForestClassifier) or isinstance(model, RandomForestRegressor):
+        if isinstance(model, RandomForestClassifier) or isinstance(
+            model, RandomForestRegressor
+        ):
             param_grid = RF_PARAM_GRID
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid,
-                               cv=3, n_jobs=-1, verbose=2)
+    grid_search = GridSearchCV(
+        estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2
+    )
     grid_search.fit(train_features, train_labels)
     return grid_search.best_estimator_
 
@@ -58,11 +75,11 @@ def optimize_hp(train_features, train_labels):
 
 
 def optimize_preproc(make_data, space):
-
     @ignore_warnings(category=ConvergenceWarning)
     def evaluate(features, labels, classifiers=[], verbose=False):
         X_train, X_test, y_train, y_test = train_test_split(
-            features, labels, test_size=0.33, random_state=69)
+            features, labels, test_size=0.33, random_state=69
+        )
         scores = []
         if len(classifiers) == 0:
             classifiers = CLASSIFIERS
@@ -72,15 +89,13 @@ def optimize_preproc(make_data, space):
             model.fit(X_train, y_train)
             scores.append((model.predict(X_test) == y_test).mean())
             if verbose:
-                iterate_through.set_description(
-                    f"{_classifier.__name__}: {scores[-1]}")
+                iterate_through.set_description(f"{_classifier.__name__}: {scores[-1]}")
         return max(scores)
 
     def objective(params):
         train_features, train_labels = make_data(params)
         return -evaluate(train_features, train_labels)
 
-    best_params = fmin(fn=objective, algo=tpe.suggest,
-                       space=space, max_evals=20)
+    best_params = fmin(fn=objective, algo=tpe.suggest, space=space, max_evals=20)
 
     return best_params
