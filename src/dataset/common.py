b5ce97dd5e9ba6dcf0880351a00ce5203e24eb9e
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2021-11-11 11:30:22 +0530

    include sensor readings from MITRE/MIT

--- src/dataset/common.py
+++ src/dataset/common.py
@@ -23,6 +23,50 @@ def read_ble_from_file(fp):
     )
 
 
+def read_sensors_from_file(fp):
+    readings = {}
+    lines = open(fp).read().split('\n')[10:]
+    for line in lines:
+        items = line.split(',')
+        anathema = ["Activity", "scenario", "environment", "subject",
+                    "session_id", "beacon", "receiver", "user_id", "Range",
+                    "Angle", "beacon_subject", "partner_tester", "self_tester",
+                    "partner_on_body_location", "partner_pose", "app_name",
+                    "app_ver", "self_on_body_location", "self_pose", "self_activity",
+                    "partner_activity", "app_name", "self_user_id", "Pedometer",
+                    "partner_beacon_id", "self_beacon_id"]
+        if not line.strip():
+            continue
+        if items[1] in anathema:
+            continue
+        if "Bluetooth" in line:
+            if "Bluetooth" not in readings:
+                readings["Bluetooth"] = []
+            readings["Bluetooth"].append(float(items[3]))
+        elif "Heading" in line:
+            sensor_type = items[1]
+            assert len(items) == 8
+            suffixes = ["x1", "y1", "z1", "x2", "y2", "z2"]
+            for suffix, scalar in zip(suffixes, items[2:]):
+                name = sensor_type + "_" + suffix
+                if name not in readings:
+                    readings[name] = []
+                readings[name].append(float(scalar))
+        else:
+            suffixes = ["x", "y", "z"]
+            sensor_type = items[1]
+            for suffix, scalar in zip(suffixes, items[2:]):
+                name = sensor_type + "_" + suffix
+                if name not in readings:
+                    readings[name] = []
+                if scalar.strip():
+                    try:
+                        readings[name].append(float(scalar))
+                    except ValueError:
+                        print(line)
+    return readings
+
+
 def apply_fn_to_data_files(_dir, fn, as_df=False):
     _apply_out = []
     folders = list(os.walk(_dir))
@@ -33,3 +77,15 @@ def apply_fn_to_data_files(_dir, fn, as_df=False):
     if as_df:
         return pd.DataFrame(_apply_out)
     return _apply_out
+
+
+def summarize_sensor(readings, name):
+    summary = []
+    for event_readings in readings:
+        _event_readings = np.array(event_readings)
+        summary.append({
+            f"{name}:Min": _event_readings.min(),
+            f"{name}:Mean": _event_readings.mean(),
+            f"{name}:Max": _event_readings.max()
+        })
+    return summary
--- src/dataset/mitre.py
+++ src/dataset/mitre.py
@@ -1,5 +1,6 @@
-from src.data.common import apply_fn_to_data_files, mitre_dir, read_ble_from_file
 import pandas as pd
+from src.dataset.common import (apply_fn_to_data_files, mitre_dir,
+                                read_sensors_from_file, summarize_sensor)
 
 MITRE_PROCESSED_DATA = "data/processed/MITRE-range_angle_structured.Bluetooth.csv"
 
@@ -23,16 +24,20 @@ def get_headers(fp):
             devices["Environment_2"] = e2.strip()
         if "Range" in line:
             devices["Range"] = float(line.split(",")[-1])
-        if "Angle" in line:
-            devices["Angle"] = float(line.split(",")[-1])
-    devices["Bluetooth"] = read_ble_from_file(fp)
-    return devices
+    readings = read_sensors_from_file(fp)
+    return devices | readings
 
 
 def make_data():
-    data = apply_fn_to_data_files(mitre_dir, get_headers, as_df=True)
-    data.to_csv(MITRE_PROCESSED_DATA, index=False)
-    return data
+    data = apply_fn_to_data_files(mitre_dir, get_headers, as_df=False)
+    non_sensor_cols = ["Environment_1", "Environment_2", "TX", "RX", "Range"]
+    sensor_cols = [col for col in data[0].keys() if col not in non_sensor_cols]
+    df = pd.DataFrame(data)  # , columns=non_sensor_cols)
+    for col in sensor_cols:
+        _summary_cols = summarize_sensor(df[col].values, col)
+        df = pd.concat([df, pd.DataFrame(_summary_cols)], axis=1)
+    df = df.dropna()
+    return df.drop(sensor_cols, axis=1)
 
 
 def get_data():
